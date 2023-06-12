4acda5638ef4108575b3e97facd532b9c2857dc9
root <amr.magdy.hassan95@gmail.com>
2023-06-12 04:31:03 -0700

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module for checking instances """
+
+
+def is_same_class(obj, a_class):
+    """ return whether instance of same class"""
+    return (isinstance(obj, a_class))
