a7a62c392c5c6e471c895b22e45fa68b4e58facc
root <amr.magdy.hassan95@gmail.com>
2023-06-12 04:44:03 -0700

    4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     """ is class or subclass"""
-    return (type(obj) is a_class or issubclass(obj, a_class))
+    return (isinstance(obj, a_class))
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" is subclass mod"""
+
+
+def inherits_from(obj, a_class):
+    """ is subclass of """
+    return (issubclass(obj, a_class))
