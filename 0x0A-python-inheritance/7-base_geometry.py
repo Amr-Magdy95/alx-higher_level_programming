d761a5d545e8c5d5d6e1f486dbecbb8a13fa1aff
root <amr.magdy.hassan95@gmail.com>
2023-06-12 05:27:42 -0700

    7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" empty base geo class"""
+
+
+class BaseGeometry:
+    """ base geo classs"""
+    def area(self):
+        """ return the area of the shape"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """ validate integer"""
+        if value not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
