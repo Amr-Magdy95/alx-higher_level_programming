517c65ee6b07b802cf97717a70ebdc641ad5db84
root <amr.magdy.hassan95@gmail.com>
2023-06-13 14:36:56 -0700

    102

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,4 +15,4 @@ class Student:
         if (isinstance(attrs, list) and
                 all(isinstance(x, str) for x in attrs)):
             return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
-        return (self.__dict__.copy())
+        return (self.__dict__)
