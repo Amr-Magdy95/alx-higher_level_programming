d7a8741340af7708594ab171f15f364bf982733e
root <amr.magdy.hassan95@gmail.com>
2023-06-13 08:14:08 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read file module"""
 
 
 def read_file(filename=""):
+    """read file function"""
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
