27b33f4defadd9e545bd2a0f687bd4ca3d81ef78
root <amr.magdy.hassan95@gmail.com>
2023-06-13 08:33:13 -0700

    1x4

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 def write_file(filename="", text=""):
     """write file function"""
-    with open(filename, 'r', encoding="utf-8") as f:
-        return (len(f.readlines()))
+    with open(filename, 'w', encoding="utf-8") as f:
+        return (f.write(text))
+        
 
