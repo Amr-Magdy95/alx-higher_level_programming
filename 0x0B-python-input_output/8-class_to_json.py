7a3dbb4036c938a1383e2fff21ab941e8cec1989
root <amr.magdy.hassan95@gmail.com>
2023-06-13 14:23:14 -0700

    8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class to json module"""
+import json
+
+
+def class_to_json(obj):
+    """class to json fn"""
+    dic = {}
+    if hasattr(obj, "__dict__"):
+        dic = obj.__dict__.copy()
+    return (dic)
