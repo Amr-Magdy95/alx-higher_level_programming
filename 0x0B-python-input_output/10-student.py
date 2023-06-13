730d2ede29aa227f1c7e8fdd6f43b262894ca00e
root <amr.magdy.hassan95@gmail.com>
2023-06-13 14:38:04 -0700

    10x3

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,7 +10,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ class dic"""
         if (isinstance(attrs, list) and
                 all(isinstance(x, str) for x in attrs)):
